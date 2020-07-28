b50450b5fbe9ead411b2beb0cdef518618058aa6
savage <nickolay.pylypenko@gmail.com>
2020-07-28 21:12:07 +0300

    Created forms, templates, full CRUD and partly created bootstrap front

--- crashcourse/todo/views.py
+++ crashcourse/todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
+from .forms import TodoForm
 # Create your views here.
 def todo_list(request):
     todos = Todo.objects.all()
@@ -17,3 +18,27 @@ def todo_detail(request, id):
         'todo': todo
     }
     return render(request,"todo/todo_detail.html", context)
+
+
+def todo_create(request):
+    form = TodoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "todo/todo_create.html", context)
+
+
+def todo_update(request, id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=todo)
+    if form.is_valid():
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "todo/todo_update.html", context)
+
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+    todo.delete()
+    return redirect("/")
